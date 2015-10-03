e71679e257d1f3c4b8acc13fe390ebc9a8aa73f4
Jasdy Syarman <akutaktau@gmail.com>
2015-10-04 01:42:20 +0800

    adding css

--- index.php
+++ index.php
@@ -1,6 +1,52 @@
 <html>
 <head>
 	<title> Config wifi</title>
+	<style>
+		div.ssid {
+			position:absolute;
+			top : 100px;
+			left : 35%;
+			background: #FFFFFF;
+			padding: 20px;
+			width: 300px;
+			height: 150px;
+			
+			margin:0px;
+			padding:10px;
+			
+		}
+		
+		label {
+			width:100px;
+			display: inline-block;
+			padding-bottom:5px;
+		}
+		
+		div.topdiv {
+			
+			border-top-right-radius: 10px;
+			border-top-left-radius: 10px;
+			background-color:#5291EA;
+			padding:10px;2
+		}
+		
+		div.centrediv{
+			border-left:2px solid;
+			border-right:2px solid;
+			border-bottom:2px solid;
+			border-color: #5291EA;
+			padding:10px;
+			border-bottom-left-radius: 10px;
+			border-bottom-right-radius: 10px;
+		}
+		
+		span > input {
+			padding : 10px;
+			margin : 10px;
+			width : 250px;
+		}
+		
+	</style
 </head>
 <body>
 <?php if(isset($_POST['submit'])) {
@@ -17,10 +63,15 @@
 		
 	}
 }?>
-<form method="POST">
-SSID : <input type="text" name="ssid" />
-Password : <input type="text" name="psk" />
-<input type="submit" name="submit" value="Add Wifi" />
-</form>
+<div class="ssid">
+	<div class="topdiv">Raspberry Pi Wifi Connection</div>
+	<div class="centrediv">
+		<form method="POST">
+			<label>SSID :</label> <input type="text" name="ssid" /><br />
+			<label>Password :</label> <input type="text" name="psk" /><br >
+			<span><input type="submit" name="submit" value="Connect to Wifi" /></span>
+		</form>
+	</div>
+</div>
 </body>
 </html>
